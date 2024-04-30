d094d9e52356e4d1ce711c06bbda6395f6bea7d1
Savannah Gropp <savagrop329@ed.amdsb.ca>
2024-04-30 10:05:57 -0400

    it works!

--- BrickBreaker/Properties/Resources.Designer.cs
+++ BrickBreaker/Properties/Resources.Designer.cs
@@ -449,6 +449,16 @@ namespace BrickBreaker.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap minecraftBkgd {
+            get {
+                object obj = ResourceManager.GetObject("minecraftBkgd", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap minecraftLogo {
             get {
                 object obj = ResourceManager.GetObject("minecraftLogo", resourceCulture);
