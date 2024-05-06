22e2e1255fbbba2e4cb623899329152c79e428e1
Savannah Gropp <savagrop329@ed.amdsb.ca>
2024-05-06 09:51:07 -0400

    sounds added

--- BrickBreaker/Screens/GameScreen.Designer.cs
+++ BrickBreaker/Screens/GameScreen.Designer.cs
@@ -40,14 +40,12 @@
             // 
             // GameScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.DoubleBuffered = true;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "GameScreen";
-            this.Size = new System.Drawing.Size(975, 674);
+            this.Size = new System.Drawing.Size(780, 539);
             this.Load += new System.EventHandler(this.GameScreen_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.GameScreen_Paint);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.GameScreen_KeyUp);
